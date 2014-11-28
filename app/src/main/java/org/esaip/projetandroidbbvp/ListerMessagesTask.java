a12f4dfd8f39b7b6312f1d77d565beaf6159b49c
Perraudeau <perraudeau.victor@gmail.com>
2014-11-28 22:48:37 +0100

    finalisation connexion

---
+++ app/src/main/java/org/esaip/projetandroidbbvp/ListerMessagesTask.java
@@ -0,0 +1,38 @@
+package org.esaip.projetandroidbbvp;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+
+/**
+ * Created by Baptiste on 28/11/2014.
+ */
+class ListerMessagesTask extends AsyncTask<String, Void, Boolean> {
+
+    interface OnTaskEvent {
+        public void onPreExecute();
+        public void onFinish(Boolean result);
+    }
+
+    OnTaskEvent onTaskEvent;
+
+    ListerMessagesTask(OnTaskEvent onTaskEvent) {
+        this.onTaskEvent = onTaskEvent;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        onTaskEvent.onPreExecute();
+    }
+
+    @Override
+    protected Boolean doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+        onTaskEvent.onFinish(aBoolean);
+    }
+}
