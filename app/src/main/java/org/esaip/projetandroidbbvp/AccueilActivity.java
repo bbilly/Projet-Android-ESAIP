b2e78c7b3bf21aae61bd4e8b5ae832fcd996d3fc
Baptiste <baptiste.billy@gmail.com>
2014-11-28 17:11:59 +0100

    Modif vue choix action + task vers les 2 activités

--- app/src/main/java/org/esaip/projetandroidbbvp/AccueilActivity.java
+++ app/src/main/java/org/esaip/projetandroidbbvp/AccueilActivity.java
@@ -8,9 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
-public class AccueilActivity extends Activity {
+public class AccueilActivity extends Activity implements ListerMessagesTask.OnTaskEvent {
     Button btn_lister = null;
     Button btn_envoyer = null;
 
@@ -18,15 +19,51 @@ public class AccueilActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-
+        //on recupere les boutons dans la vue grace à leurs ids
         btn_lister = (Button) findViewById(R.id.btn_lister);
         btn_envoyer = (Button) findViewById(R.id.btn_envoyer);
 
+        //evenement lors du clic sur le bouton lister messages
         btn_lister.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                new ListerMessagesTask().execute();
+                //on appelle la tache pour lister les messages
+                new ListerMessagesTask(new ListerMessagesTask.OnTaskEvent() {
+                    @Override
+                    public void onPreExecute() {
+                        //afficher la roue
+                    }
+
+                    @Override
+                    public void onFinish(Boolean result) {
+                        //lancer ou pas l'activité
+                        Intent intent = new Intent(getApplicationContext(), ListerMessagesActivity.class);
+                        startActivity(intent);
+                    }
+                }).execute();
             }
         });
+
+        //evenemet lors du clic sur le bouton envoyer message
+        btn_envoyer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new EnvoyerMessageTask(new EnvoyerMessageTask.OnTaskEvent() {
+                    @Override
+                    public void onPreExecute() {
+
+                    }
+
+                    @Override
+                    public void onFinish(Boolean result) {
+                        Intent intent = new Intent(getApplicationContext(), EnvoyerMessageActivity.class);
+                        startActivity(intent);
+                    }
+                }).execute();
+
+            }
+        });
+
+
     }
 
 
@@ -52,22 +89,14 @@ public class AccueilActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    class ListerMessagesTask extends AsyncTask<String, Void, Boolean> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
 
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            return null;
-        }
+    @Override
+    public void onPreExecute() {
 
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-            Intent intent = new Intent(getApplicationContext(), ListerMessagesActivity.class);
-            startActivity(intent);
-        }
+    }
+
+    @Override
+    public void onFinish(Boolean result) {
+
     }
 }
